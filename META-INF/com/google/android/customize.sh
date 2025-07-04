4d04a18a9baf2c8108d30507e813a8233bd75b3d
StarPikicito <jnsapy@gmail.com>
2025-07-04 01:22:34 -0300

    Add files via upload

---
+++ META-INF/com/google/android/customize.sh
@@ -0,0 +1,3 @@
+#!/system/bin/sh
+MODPATH=${0%/*}
+cp -af $MODPATH/system/* /system
---
+++ META-INF/com/google/android/install.sh
@@ -0,0 +1,3 @@
+#!/system/bin/sh
+unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+set_perm_recursive $MODPATH 0 0 0755 0644
